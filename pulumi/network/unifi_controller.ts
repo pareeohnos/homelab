13cb1db4fecb29547c05d1d3d8cb546a9f989201
Adrian Hooper <git@ca.hooper.co.uk>
2025-06-22 21:11:24 +0100

    Change memory of unifi controller

--- pulumi/network/unifi_controller.ts
+++ pulumi/network/unifi_controller.ts
@@ -6,17 +6,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as proxmox from "@muhlba91/pulumi-proxmoxve";
 import provider from "./provider";
-import { Hosts, HostsConfiguration, ProxmoxNodeNames } from "../types";
+import { Hosts, HostsConfiguration, ProxmoxNodeNames, ProxmoxConfiguration } from "../types";
 import { buildLxcConfiguration } from "./lxc_config";
 
 const config = new pulumi.Config();
 
 const hostsConfig = config.requireObject<HostsConfiguration>("hosts");
+const proxmoxConfig = config.requireObject<ProxmoxConfiguration>("proxmox");
 
 export const unifiControllerContainer = new proxmox.ct.Container(
   hostsConfig.unifiController.hostname,
   buildLxcConfiguration(ProxmoxNodeNames.NETWORKING, Hosts.UNIFI, {
     description: "Unifi controller",
+    memory: {
+      dedicated: 2048
+    }
   }),
   {
     provider,
